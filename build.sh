3f14e6606aea7686c349a6e155507b776bae0aaa
bhanuprasad0722 <bhanuprasadchellapuram>
2025-12-19 14:58:38 +0530

    updated build command

--- build.sh
+++ build.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 set -o errexit
 
+echo "Installing dependencies..."
 pip install -r requirements.txt
-python manage.py migrate
+
+echo "Running migrations..."
+python config/manage.py migrate --noinput
+
+echo "Collecting static files..."
+python config/manage.py collectstatic --noinput
