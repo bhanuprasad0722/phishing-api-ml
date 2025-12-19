3b6d879396918d32b5e18fa6604533f11c24204b
bhanuprasad0722 <bhanuprasadchellapuram>
2025-12-19 18:44:53 +0530

    changed model path again

--- config/predictions/ml_model.py
+++ config/predictions/ml_model.py
@@ -3,9 +3,12 @@ import joblib
 from django.conf import settings
 from ml.feature_extraction import extract_features
 
-# settings.BASE_DIR -> /opt/render/project/src/config
-MODEL_PATH = os.path.join(settings.BASE_DIR, "..", "ml", "artifacts", "random_forest_model.pkl")
-MODEL_PATH = os.path.abspath(MODEL_PATH)
+MODEL_PATH = os.path.join(
+    settings.BASE_DIR,
+    "ml",
+    "artifacts",
+    "random_forest_model.pkl"
+)
 
 print("ML MODEL PATH:", MODEL_PATH)  # Optional: check path in logs
 
